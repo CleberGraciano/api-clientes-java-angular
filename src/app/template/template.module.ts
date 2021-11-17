350eb03caa91a60bb87c6cfbdc95ee6ad9d321c0
Cleber <cleber_batista2010@hotmail.com>
2021-11-17 19:07:06 -0300

    Concluindo component Navbar

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -10,6 +10,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     CommonModule
+  ], 
+  exports: [
+    NavbarComponent
   ]
 })
 export class TemplateModule { }
