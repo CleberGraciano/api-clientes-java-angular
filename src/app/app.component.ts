0f24cf85c3e9ff6da0d81bbff8dbf8f811afdb76
Cleber <cleber_batista2010@hotmail.com>
2021-11-17 20:34:13 -0300

    Adicionando Script para funcionar o botão Toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(){
+    (function($) {
+      "use strict";
+      let path : any;
+  
+      // Add active state to sidbar nav links
+      path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+              if (window.location.href === path) {
+                  $().addClass("active");
+              }
+          });
+  
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e:any) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+  })(jQuery);
+  }
+  
   title = 'clientes-app';
+
+  
 }
